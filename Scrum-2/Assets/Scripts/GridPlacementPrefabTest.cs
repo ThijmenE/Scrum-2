d41b809db9c56f90a4e8dbac7d42e226e013dc14
ThijmenE <taeggens@st.noorderpoort.nl>
2026-03-17 09:59:47 +0100

    TestScene plus Testscript added

--- Scrum-2/Assets/Scripts/GridPlacementPrefabTest.cs
+++ Scrum-2/Assets/Scripts/GridPlacementPrefabTest.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class GridPlacementPrefabTest : MonoBehaviour
+public class GridPlacement : MonoBehaviour
 {
     public Tilemap tilemap;
     public Tile[] tiles;
